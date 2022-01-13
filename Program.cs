9f098db1e2aa3df51689ff24b1b4b7019792f886
samantha solomon <ssolomon2024@chadwickschool.org>
2022-01-13 11:52:12 -0800

    chore: Initialize Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ssolomon2024_DataEncodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
