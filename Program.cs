dbcf0f7938724499b92b1e86486f663ad5c9aa68
samantha solomon <ssolomon2024@chadwickschool.org>
2022-01-18 10:32:28 -0800

    worked on decrypting all files

--- Program.cs
+++ Program.cs
@@ -6,16 +6,41 @@ namespace ssolomon2024_DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            Cipher cipher;
-            cipher = new Cipher(5);
+            string filePath = args[0];
+            Console.WriteLine($"Loading '{filePath}'.");
+            
+            // Cipher cipher;
+            // cipher = new Cipher(5);
 
-            string encrypted;
-            encrypted = cipher.Encrypt("rosebud");
-            Console.WriteLine($"The encrypted message is: '{encrypted}'");
+            // string encrypted;
+            // encrypted = cipher.Encrypt("rosebud");
+            // Console.WriteLine($"The encrypted message is: '{encrypted}'");
+
+            // string decrypted;
+            // decrypted = cipher.Decrypt(encrypted);
+            // Console.WriteLine($"The decrypted message is: '{decrypted}'");
+            
+            string message;
+            message = System.IO.File.ReadAllText(filePath);
+            Console.WriteLine($"The encrypted message is: '{message}'");
 
             string decrypted;
-            decrypted = cipher.Decrypt(encrypted);
-            Console.WriteLine($"The decrypted message is: '{decrypted}'");
+            int shift;
+            shift = 1;
+            while (shift <= 10)
+            {
+            Cipher cipher;
+            cipher = new Cipher(shift);
+            decrypted = cipher.Decrypt(message);
+            Console.WriteLine($"The decrypted message with shift {shift} is:'{decrypted}'");
+            shift = shift + 1;
+            }
+
+            // Cipher cipher;
+            // cipher = new Cipher(3);
+            // decrypted = cipher.Decrypt(message);
+            // Console.WriteLine($"The decrypted message is:'{decrypted}'");
+
         }
     }
 }
